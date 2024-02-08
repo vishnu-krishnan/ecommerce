7c04fef1501f8b2dd3fb81bd43e8abd073cc1ddc
vishnu-krishnan <vishnuoffcl@gmail.com>
2024-02-08 17:18:28 +0530

    added logback files

--- gateway-service/src/main/java/com/aithmetic/gateway/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/aithmetic/gateway/config/GatewayConfig.java
@@ -1,7 +1,6 @@
 package com.aithmetic.gateway.config;
 
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -9,18 +8,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @Configuration
 public class GatewayConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GatewayConfig.class);
 
     @Autowired
     private Environment environment;
 
     @Bean
-    public RouteLocator customRouteLocator(@NotNull RouteLocatorBuilder routeLocatorBuilder){
+    public RouteLocator customRouteLocator( RouteLocatorBuilder routeLocatorBuilder){
 
         String customerServiceUrl = environment.getProperty("customer.service.url");
         String productServiceUrl = environment.getProperty("product.service.url");
         String orderServiceUrl = environment.getProperty("order.service.url");
+
+        LOGGER.info("Configuring routes for customer-service: {}", customerServiceUrl);
+        LOGGER.info("Configuring routes for product-service: {}", productServiceUrl);
+        LOGGER.info("Configuring routes for order-service: {}", orderServiceUrl);
 
         return routeLocatorBuilder.routes()
                 .route("customer-service", r->r
